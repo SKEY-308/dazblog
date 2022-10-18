eacde247cbc515868bdc8ef9cf796a1427722266
Severus KEY <severus.key308@gmail.com>
2022-10-18 19:27:13 +0200

    28th Com

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 
 import { submitComment } from '../services';
 
@@ -7,37 +7,43 @@ export default function CommentsForm({ slug }) {
     const [error, setError] = useState(false);
     const [localStorage, setLocalStorage] = useState(null);
     const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-    const [formData, setFormData] = useState({ name: '', email: '', comment: '', storeData: false });
+    const commentEl = useRef();
+    const nameEl = useRef();
+    const emailEl = useRef();
+    const storeDataEl = useRef();
 
-    useEffect(() => {
-        setLocalStorage(window.localStorage);
-        const initalFormData = {
-            name: window.localStorage.getItem('name'),
-            email: window.localStorage.getItem('email'),
-            storeData: window.localStorage.getItem('name') || window.localStorage.getItem('email'),
-        };
-        setFormData(initalFormData);
-    }, []);
+    // useEffect(() => {
+    //     setLocalStorage(window.localStorage);
+    //     const initalFormData = {
+    //         name: window.localStorage.getItem('name'),
+    //         email: window.localStorage.getItem('email'),
+    //         storeData: window.localStorage.getItem('name') || window.localStorage.getItem('email'),
+    //     };
+    //     setFormData(initalFormData);
+    // }, []);
 
-    const onInputChange = (e) => {
-        const { target } = e;
-        if (target.type === 'checkbox') {
-            setFormData((prevState) => ({
-                ...prevState,
-                [target.name]: target.checked,
-            }));
-        } else {
-            setFormData((prevState) => ({
-                ...prevState,
-                [target.name]: target.value,
-            }));
-        }
-    };
+    // const onInputChange = (e) => {
+    //     const { target } = e;
+    //     if (target.type === 'checkbox') {
+    //         setFormData((prevState) => ({
+    //             ...prevState,
+    //             [target.name]: target.checked,
+    //         }));
+    //     } else {
+    //         setFormData((prevState) => ({
+    //             ...prevState,
+    //             [target.name]: target.value,
+    //         }));
+    //     }
+    // };
 
-    const handlePostSubmission = () => {
+    const handleCommentSubmission = () => {
         setError(false);
 
-        const { name, email, comment, storeData } = formData;
+        const { value: comment } = commentEl.current;
+        const { value: name } = nameEl.current;
+        const { value: email } = emailEl.current;
+        const { checked: storeData } = storeDataEl.current;
 
         if (!name || !email || !comment) {
             setError(true);
@@ -78,26 +84,38 @@ export default function CommentsForm({ slug }) {
     return (
         <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
             <h3 className="text-xl mb-8 font-semibold border-b pb-4">Leave a Reply</h3>
+
             <div className="grid grid-cols-1 gap-4 mb-4">
-                <textarea value={ formData.comment } onChange={ onInputChange } className="p-4 outline-none w-full rounded-lg h-40 focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700" name="comment" placeholder="Comment" />
+                <textarea ref={ commentEl } className="p-4 outline-none w-full rounded-lg h-40 focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700" name="comment" placeholder="Comment" />
             </div>
+
             <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mb-4">
-                <input type="text" value={ formData.name } onChange={ onInputChange } className="py-2 px-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700" placeholder="Name" name="name" />
-                <input type="email" value={ formData.email } onChange={ onInputChange } className="py-2 px-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700" placeholder="Email" name="email" />
+
+                <input type="text" ref={ nameEl } className="py-2 px-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700" placeholder="Name" name="name" />
+
+                <input type="email" ref={ emailEl } className="py-2 px-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700" placeholder="Email" name="email" />
+
             </div>
+
             <div className="grid grid-cols-1 gap-4 mb-4">
                 <div>
-                    <input checked={ formData.storeData } onChange={ onInputChange } type="checkbox" id="storeData" name="storeData" value="true" />
-                    <label className="text-gray-500 cursor-pointer ml-2" htmlFor="storeData"> Save my name, email in this browser for the next time I comment.</label>
+                    <input ref={ storeDataEl } type="checkbox" id="storeData" name="storeData" value="true" />
+
+                    <label className="text-gray-500 cursor-pointer ml-2" htmlFor="storeData">
+                        Save my Name, Email in this browser for the next time I comment.
+                    </label>
                 </div>
             </div>
+
             { error && <p className="text-xs text-red-500">All fields are required</p> }
+
             <div className="mt-8">
-                <button type="button" onClick={ handlePostSubmission } className="transition duration-500 ease hover:bg-indigo-900 inline-block bg-blue-600 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer">
+                <button type="button" onClick={ handleCommentSubmission } className="transition duration-500 ease hover:bg-indigo-900 inline-block bg-blue-600 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer">
                     Post Comment
                 </button>
                 { showSuccessMessage && <span className="text-xl float-right font-semibold mt-3 text-green-500">Comment submitted for review</span> }
             </div>
+
         </div>
     )
 }
--- pages/api/comments.js
+++ pages/api/comments.js
@@ -3,6 +3,7 @@
 import { GraphQLClient, gql } from 'graphql-request';
 
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
+const graphcmsToken = process.env.GRAPHCMS_TOKEN
 
 /** *************************************************************
 * Any file inside the folder pages/api is mapped to /api/* and  *
@@ -10,10 +11,13 @@ const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
 *************************************************************** */
 
 // export a default function for API route to work
-export default async function asynchandler(req, res) {
+export default async function comments(req, res) {
+
+  // const { name, email, comment, slug } = req.body;
+
   const graphQLClient = new GraphQLClient((graphqlAPI), {
     headers: {
-      authorization: `Bearer ${process.env.GRAPHCMS_TOKEN}`,
+      authorization: `Bearer ${graphcmsToken}`,
     },
   });
 
@@ -23,12 +27,13 @@ export default async function asynchandler(req, res) {
     }
   `;
 
-  const result = await graphQLClient.request(query, {
-    name: req.body.name,
-    email: req.body.email,
-    comment: req.body.comment,
-    slug: req.body.slug,
-  });
+  try {
+    const result = await graphQLClient.request(query, req.body);
 
-  return res.status(200).send(result);
+    return res.status(200).send(result);
+  }
+  catch (error) {
+    return res.status(500).send(error);
+  }
+
 }
