864da19c30cefd11e4967a98313fef128596d610
Severus KEY <severus.key308@gmail.com>
2022-10-18 20:42:53 +0200

    29th Com

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -18,11 +18,13 @@ export default function Comments({ slug }) {
         <div>
             { comments.length > 0 && (
                 <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
+
                     <h3 className="text-xl mb-8 font-semibold border-b pb-4">
                         { comments.length }
                         { ' ' }
                         Comments
                     </h3>
+
                     { comments.map((comment, index) => (
                         <div key={ index } className="border-b border-gray-100 mb-4 pb-4">
                             <p className="mb-4">
@@ -35,6 +37,7 @@ export default function Comments({ slug }) {
                             <p className="whitespace-pre-line text-gray-600 w-full">{ parse(comment.comment) }</p>
                         </div>
                     )) }
+
                 </div>
             ) }
         </div>
